a5e48f671cb56809ae28fce68294d3cc257fbae6
Tanmay Vijayvargiya <tanmay.vijayvargiya@sap.com>
2018-09-14 17:54:42 +0530

    fullscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import screenfull from 'screenfull';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'home-dashboard';
+
+  ngOnInit() {
+    // setFullScreen(true);
+  }
+
+  goFullscreen() {
+    toggleFullScreen();
+  }
+}
+
+interface FsDocument extends HTMLDocument {
+  mozFullScreenElement?: Element;
+  msFullscreenElement?: Element;
+  msExitFullscreen?: () => void;
+  mozCancelFullScreen?: () => void;
+}
+
+function isFullScreen(): boolean {
+  const fsDoc = <FsDocument> document;
+
+  return !!(fsDoc.fullscreenElement || fsDoc.mozFullScreenElement || fsDoc.webkitFullscreenElement || fsDoc.msFullscreenElement);
+}
+
+interface FsDocumentElement extends HTMLElement {
+  msRequestFullscreen?: () => void;
+  mozRequestFullScreen?: () => void;
+}
+
+function toggleFullScreen(): void {
+  const fsDoc = <FsDocument> document;
+
+  if (!isFullScreen()) {
+    const fsDocElem = <FsDocumentElement> document.documentElement;
+
+    if (fsDocElem.requestFullscreen)
+      fsDocElem.requestFullscreen();
+    else if (fsDocElem.msRequestFullscreen)
+      fsDocElem.msRequestFullscreen();
+    else if (fsDocElem.mozRequestFullScreen)
+      fsDocElem.mozRequestFullScreen();
+    else if (fsDocElem.webkitRequestFullscreen)
+      fsDocElem.webkitRequestFullscreen();
+  }
+  else if (fsDoc.exitFullscreen)
+    fsDoc.exitFullscreen();
+  else if (fsDoc.msExitFullscreen)
+    fsDoc.msExitFullscreen();
+  else if (fsDoc.mozCancelFullScreen)
+    fsDoc.mozCancelFullScreen();
+  else if (fsDoc.webkitExitFullscreen)
+    fsDoc.webkitExitFullscreen();
+}
+
+function setFullScreen(full: boolean): void {
+  if (full !== isFullScreen())
+    toggleFullScreen();
 }
